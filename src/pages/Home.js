8af57d4df058792ec01a5ae211ee8c3f7c9cedae
Ziyed1 <zhammadi@sollan.com>
2023-05-19 12:37:18 +0200

    Final Change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+//components - Template pour les documents
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
 const Home = () => {
+    const {workouts, dispatch} = useWorkoutsContext()
+
+    // Sert a gérer les effets de bord, et peut effectuer des tâches comme charger des données ou autre.
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            // fetch = GET par défaut
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        fetchWorkouts()
+    }, [dispatch])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    // Utilisation du template
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
+            <WorkoutForm />
         </div>
     )
 }
